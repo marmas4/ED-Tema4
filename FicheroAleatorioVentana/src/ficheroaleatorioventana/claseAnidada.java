b1e750610fd4a4a4e7d86442fad6674f1c6c04ac
Miguel Armas <marmas4@hotmail.com>
2020-05-25 22:57:48 +0100

    Cambios Actividad 4 del Tema 4 de ED Add clase anidada

---
+++ FicheroAleatorioVentana/src/ficheroaleatorioventana/claseAnidada.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficheroaleatorioventana;
+
+/**
+ *
+ * @author miguel alfredo armas chávez
+ */
+class claseAnidada {
+    
+    void entrada() {
+        System.out.println("Método entrada.");
+    }
+
+    String salida(int d) {
+        System.out.println("Salida.");
+        return "Salida el " + d;
+    }
+    
+}
